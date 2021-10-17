e33d5f5ef66f311563a9f94c98e03045ec387bb9
VU Quang Minh <vuquangminh2000317@gmail.com>
2021-10-17 19:40:36 +0800

    Created spring project

---
+++ yelp_project/yelp_project/src/main/java/com/cs/yelp_project/YelpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cs.yelp_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelpProjectApplication.class, args);
+	}
+
+}
