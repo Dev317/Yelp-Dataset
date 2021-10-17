5724484f48aa0ff003521c2d5cc9e73002bf919f
VU Quang Minh <vuquangminh2000317@gmail.com>
2021-10-17 21:34:33 +0800

    Created parser for Check In

--- src/main/java/com/cs/yelp_project/YelpProjectApplication.java
+++ src/main/java/com/cs/yelp_project/YelpProjectApplication.java
@@ -2,6 +2,8 @@ package com.cs.yelp_project;
 
 import com.cs.yelp_project.business.Business;
 import com.cs.yelp_project.business.BusinessService;
+import com.cs.yelp_project.checkin.CheckIn;
+import com.cs.yelp_project.checkin.CheckInService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,24 +24,43 @@ public class YelpProjectApplication {
 		System.out.println("started");
 	}
 
+//	@Bean
+//	CommandLineRunner runnerBusiness(BusinessService businessService) {
+//		return args -> {
+//
+//			ObjectMapper mapper = new ObjectMapper();
+//
+//			// In case any unknown values are not registered
+////			mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+//			mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+//
+//			TypeReference<List<Business>> typeReference = new TypeReference<List<Business>>(){};
+//
+//			InputStream inputStream = TypeReference.class.getResourceAsStream("/business/business_dataset.json");
+//
+//			try {
+//				List<Business> businesses = mapper.readValue(inputStream,typeReference);
+//				businessService.save(businesses);
+//				System.out.println("Businesses Saved!");
+//			} catch (IOException e) {
+//				System.out.println(e.getMessage());
+//			}
+//		};
+//	}
+
 	@Bean
-	CommandLineRunner runnerBusiness(BusinessService businessService) {
+	CommandLineRunner runnerCheckIn(CheckInService checkinService) {
 		return args -> {
 
 			ObjectMapper mapper = new ObjectMapper();
+			TypeReference<List<CheckIn>> typeReference = new TypeReference<List<CheckIn>>(){};
 
-			// In case any unknown values are not registered
-//			mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-			mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
-
-			TypeReference<List<Business>> typeReference = new TypeReference<List<Business>>(){};
-
-			InputStream inputStream = TypeReference.class.getResourceAsStream("/business/business_dataset.json");
+			InputStream inputStream = TypeReference.class.getResourceAsStream("/checkin/checkin_dataset.json");
 
 			try {
-				List<Business> businesses = mapper.readValue(inputStream,typeReference);
-				businessService.save(businesses);
-				System.out.println("Businesses Saved!");
+				List<CheckIn> checkIns = mapper.readValue(inputStream,typeReference);
+				checkinService.save(checkIns);
+				System.out.println("Check-Ins Saved!");
 			} catch (IOException e) {
 				System.out.println(e.getMessage());
 			}
---
+++ src/main/java/com/cs/yelp_project/checkin/CheckInService.java
@@ -0,0 +1,28 @@
+package com.cs.yelp_project.checkin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CheckInService {
+    private CheckInRepository repository;
+
+    @Autowired
+    public CheckInService(CheckInRepository repository) {
+        this.repository = repository;
+    }
+
+    public Iterable<CheckIn> list() {
+        return repository.findAll();
+    }
+
+    public Iterable<CheckIn> save(List<CheckIn> list) {
+        return repository.saveAll(list);
+    }
+
+    public CheckIn save(CheckIn checkIn) {
+        return repository.save(checkIn);
+    }
+}
