a2cff1852372d8d6d3c245305108ebafc4feb7f3
Minhhhh <70529335+Dev317@users.noreply.github.com>
2021-10-17 20:11:26 +0800

    Delete yelp_project/yelp_project directory

--- yelp_project/yelp_project/src/main/java/com/cs/yelp_project/business/BusinessRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cs.yelp_project.business;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BusinessRepository extends JpaRepository<Business,String> {
-}
--- yelp_project/yelp_project/src/main/java/com/cs/yelp_project/checkin/CheckInRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cs.yelp_project.checkin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CheckInRepository extends JpaRepository<CheckIn, String> {
-
-}
