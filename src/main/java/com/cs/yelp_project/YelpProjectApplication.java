2dac4efe09e3a64a0e5eeb0301fea70b625af312
jaydxn1 <pangjunrong99@gmail.com>
2021-10-28 22:37:56 +0800

    optimised parsing of information

--- src/main/java/com/cs/yelp_project/YelpProjectApplication.java
+++ src/main/java/com/cs/yelp_project/YelpProjectApplication.java
@@ -5,7 +5,6 @@ import com.cs.yelp_project.business.BusinessService;
 import com.cs.yelp_project.business.Category;
 import com.cs.yelp_project.business.CategoryService;
 import com.cs.yelp_project.checkin.CheckIn;
-import com.cs.yelp_project.checkin.CheckInRepository;
 import com.cs.yelp_project.checkin.CheckInService;
 
 import com.cs.yelp_project.citystate.CityState;
@@ -21,6 +20,7 @@ import org.springframework.context.annotation.Bean;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class YelpProjectApplication {
@@ -43,63 +43,41 @@ public class YelpProjectApplication {
 		CategoryService categoryService = ctx.getBean(CategoryService.class);
 
 		List<CityState> cityStateList = new ArrayList<>();
-		List<Category> categoryList = new ArrayList<>();
 		List<Business> businessList = businessService.list();
 
-		// findTotalCheckin in each business
-		int counter = 0;
+		//Everything Takes About 3 Minutes To Run!
 		List<CheckIn> checkInList = checkInService.list();
-		for (CheckIn checkIn : checkInList) {
 
-			String businessId = checkIn.getBusiness_id();
+        Map<String, CheckIn> checkInMap = new HashMap<>();
+        for(CheckIn checkIn : checkInList){
+            CheckIn current = checkInMap.get(checkIn.getBusiness_id());
+           if(current == null){
+                checkInMap.put(checkIn.getBusiness_id(), checkIn);
+				
+           }else{
+                current.setTotal_checkin(checkIn.getTotal_checkin() + current.getTotal_checkin());
+           }
+        }
 
-			for (Business business : businessList) {
-				if (counter > checkInList.size()) {
-					System.out.println("Repeat ALERT!!!");
-					break;
-				}
-
-				if (counter < checkInList.size() && businessId.contains(business.getBusiness_id())) {
-					business.setTotalCheckin(checkIn.getTotal_checkin());
-					counter++;
-					break;
-				}
+        for (Business business: businessList) {
+			if (checkInMap.containsKey(business.getBusiness_id())) {
+				CheckIn checkIn = checkInMap.get(business.getBusiness_id());
+				business.setTotalCheckin(checkIn.getTotal_checkin());
+			} else {
+				business.setTotalCheckin(0);
 			}
 		}
-
 
 		// Getting all the categories and store into database
-		List<String> categoriesWord = new ArrayList<>();
-		for (Business business : businessList) {
-			String[] categories = parse(business.getCategories());
-
-			for (String cat:categories) {
-				if (!categoriesWord.contains(cat)) {
-					categoriesWord.add(cat);
-				}
-			}
-		}
-
-		for (String cat: categoriesWord) {
-			categoryService.save(new Category(cat));
-		}
+		categoryService.save(new Category("Mexican"));
+		categoryService.save(new Category("New Mexican Cuisine"));
+		categoryService.save(new Category("Nightlife"));
 
 		// Getting all the states
-		List<String> cityNameList = new ArrayList<>();
-		for (Business business : businessList) {
-			String cityName =  business.getCity();
-
-			if (!cityNameList.contains(cityName)) {
-				cityNameList.add(cityName);
-				cityStateList.add( new CityState(cityName));
-			}
+		List<String> cityNameList = businessList.stream().map( Business::getCity ).distinct().collect( Collectors.toList() );
+		for (String cityName: cityNameList) {
+			cityStateList.add(new CityState(cityName));
 		}
-
-//		String result = "";
-//		for (CityState cityState : cityStateList) {
-//			result += cityState.getName() + ",";
-//		}
-//		System.out.println(result);
 
 		// Getting all the businesses in each state
 		Map<String, List<Business>> map = new HashMap<>();
@@ -140,75 +118,44 @@ public class YelpProjectApplication {
 			}
 		}
 
-		// test
-//		for (CityState city : cityStateList) {
-//			List<Business> cityBusinessList = city.getBusinessList();
-//			String result = "" + city.getName() + ":";
-//			for (Business business: cityBusinessList) {
-//				result += business.getName() + " ";
-//			}
-//			System.out.println(result);
-//		}
-//
 //		 Getting all the categories in each cityState
 		for (CityState cityState : cityStateList) {
 			Map<String,Integer> frequencyMap = new TreeMap<String,Integer >();
 			List<Business> businessesInCityState = cityState.getBusinessList();
 			List<String> categoriesInCityState = new ArrayList<>();
+			categoriesInCityState.add("Mexican");
+			categoriesInCityState.add("New Mexican Cuisine");
 			List<Category> categoriesList = new ArrayList<>();
 
 			for (Business business : businessesInCityState) {
 				int totalCheckin = business.getTotalCheckin();
 
-				List<String> businessCategoryList = new ArrayList<>();
-
 				String[] categories = parse(business.getCategories());
 
 				for (String category : categories) {
-					businessCategoryList.add(category);
-				}
-
-				for (String category: businessCategoryList) {
-					if (!categoriesInCityState.contains(category)) {
-						categoriesInCityState.add(category);
+					if (category.equals("Mexican") || category.equals("New Mexican Cuisine")) {
+						if (!frequencyMap.containsKey(category)) {
+							frequencyMap.put(category,totalCheckin);
+						} else {
+							int value = frequencyMap.get(category);
+							frequencyMap.put(category, value + totalCheckin);
+						}
 					}
-
-					if (!frequencyMap.containsKey(category)) {
-						frequencyMap.put(category,totalCheckin);
-					} else {
-						int value = frequencyMap.get(category);
-						frequencyMap.put(category, value + totalCheckin);
-					}
-
 				}
 			}
 
-			for (String category : categoriesInCityState) {
-				categoriesList.add(new Category(category));
-			}
-
-
-//			frequencyMap = valueSort(frequencyMap);
+			categoriesList.add(new Category("Mexican"));
+			categoriesList.add(new Category("New Mexican Cuisine"));
 
 			cityState.setCategoryList(categoriesList);
 			cityState.setCategoryFrequency(frequencyMap);
 		}
 
 		// Testing
-//		for (CityState cityState : cityStateList) {
-//			List<Category> testCategory = cityState.getCategoryList();
-//			String testResult = "" + cityState.getName() + ":";
-//			for (Category category : testCategory ) {
-//				testResult += category.getName();
-//			}
-//			System.out.println(testResult);
-//		}
-
-		// Testing
 		System.out.println("Sorted by categories:");
 		for (int idx = 0; idx < 5; idx++) {
 			CityState cityState = cityStateList.get(idx);
-			List frequencyList = sortMap(cityState);
+			List<Map.Entry<String,Integer>> frequencyList = sortMap(cityState);
 			System.out.println(cityState.getName());
 			System.out.println(frequencyList);
 		}
